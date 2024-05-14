0654e44b2ec87fe8940723cd75d26820a7e37b30
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-14 16:20:00 +0530

    2nd script

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -2,7 +2,7 @@
 
 #arrays can store multiple values in a variable
 fruits=("mango" "apple" "banana" "grapes")
-echo " first variable: $fruits[0]"
-echo "second variable: $fruits[1]"
-echo " third variable: $fruits[2]"
-echo " fourth variable: $fruits[3]"
+echo " first variable: ${fruits[0]}"
+echo "second variable: ${fruits[1]}"
+echo " third variable: ${fruits[2]}"
+echo " fourth variable: ${fruits[3]}"
