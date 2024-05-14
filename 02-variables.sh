63537929743c11bfed161075dc9331a339c429f6
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-14 15:49:32 +0530

    2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -7,5 +7,12 @@ echo "$person1: HI gm"
 echo "$person2: hi vm"
 echo "$person1: hw r u"
 echo "$person2: im fine"
+# arguments
+person1=$1
+person2=$2
+echo "$person1: Hi this passing argments in command line"
+echo "$person2: hi yes this i this is working"
+echo "$person1: hw r u"
+echo "$person2: im fine"
 DATE=$(date)
 echo "script started executing s: ${DATE}"
