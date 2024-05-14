7cc737a36f8871fae877f78f20900c55f4c379ed
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-14 16:00:25 +0530

    2nd script

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,3 +5,7 @@
 echo " Please enter username"
 read USERNAME
 echo " username : ${USERNAME}"
+
+echo " please enter password"
+read -s PASSWORD # -s this option will hide the value entered on the terminal
+echo " password : ${PASSWORD}"
