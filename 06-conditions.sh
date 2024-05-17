8b4d6c154ea9b77ba69cfdcc8243a411e06111e1
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-17 12:21:37 +0530

    conditions

---
+++ 06-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#conditions in shell script
+number=$1
+if[$number -gt 100] # -gt greaterthan -lt lessthan -ge greater than equal -le less than equal
+then
+    echo "given number is ${number} greater than 100"
+else
+    echo "given number is ${number} less than 100"
+fi
+
