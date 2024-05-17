6f4886cb45edeb7e425a2c45a0f0d275e06f6916
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-17 13:29:58 +0530

    conditions

---
+++ 08-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+Validate(){
+    if [ $1 -ne 0]
+    then
+        echo "$2 failed"s
+    else 
+        echo "$2 failed"
+}
+if [ $ID -ne 0 ]
+then
+    echo " Error: Please login as root user to install "
+    exit 1
+else
+    echo " you are root user "
+fi 
+
+yum install mysql -y
+
+Validate $? "installing mysql"
+
+yum install git -y
+
+Validate $? "installing git"
