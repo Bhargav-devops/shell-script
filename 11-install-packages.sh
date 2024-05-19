c17a740fc58c6dc4aef459ee9c39b4bb63869e6b
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-19 13:36:13 +0530

    install packages

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ID=$(id -u)
-
-if [ ID -ne 0 ]
-then 
-    echo "Error please login as root user"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $ID -ne 0 ]
+then
+    echo -e " $R Error: Please login as root user to install $N"
     exit 1
 else
-    echo " Your are root user"
-fi
+    echo -e "you are root user"
+fi 
