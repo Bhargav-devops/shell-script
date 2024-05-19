ca1cab8e948116cc98719793bf74ffdd6f6cb8a3
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-19 14:04:37 +0530

    install packages

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -3,7 +3,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\E[33m]"
 N="\e[0m"
+
+TimeStamp=$(date +%F-%H-%M-%S)
+LogFile="/tmp/$0-$TimeStamp.log"
+
+echo " Script starte executing at $TimeStamp " &>> $LogFile
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " $2 $R failed $N "
+    else 
+        echo -e " $2  $G success $N "
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e " $R Error: Please login as root user to install $N"
@@ -12,3 +27,16 @@ else
     echo -e "you are root user"
 fi 
 echo " all arguments arguments passed $@ "
+
+for package in $@
+do
+    yum list installed ${package} &>> $LogFile
+    if [ $? -ne 0 ]
+    then
+        echo "installing the ${package}"
+        yum install ${package} -y &>> $LogFile
+        Validate $? " Installing ${package} "
+    else
+        echo -e " $Y Already ${package} is  installed $N "
+    fi
+done
