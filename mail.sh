33c0707e7d8b17b4e857ea2a56ccebe3ee3fe03c
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-27 20:58:48 +0530

    file name change

--- mail.sh
+++ mail.sh
@@ -9,4 +9,4 @@ Subject=$5
 
 FinalBody=$(sed -e "s/To_Team/$To_Team/g" -e "s/Alert/$AlertType/g" -e "s/Body/$EscapeBody/g" template.html)
 
-echo "$FinalBody" | mail -s "$(echo -e "$Subject\nContent-Type: text/html")" $To_address
+echo "$FinalBody" | mail -s "$(echo -e "$Subject\nContent-Type: text/html")" "$To_address"
