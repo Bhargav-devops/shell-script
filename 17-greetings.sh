d7ab928142e38f24233a682efcd65b7bb762547f
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-30 22:15:23 +0530

    greeting script

--- 17-greetings.sh
+++ 17-greetings.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-person=$1
+name=""
+wishes=""
 
-echo "hello $person er are learning shell"
+Usage(){
+echo " usage $(vasename $0) -n <name> -w <wishes> "   
+echo "options to use"
+echo "-n : give name (mandatory)"
+echo "-w: provide wishes"
+echo "-h: display help with text"
+
+}
+
+while getopts ":n:w:h:" opt; do
+    case $opt in
+        n) name="$OPTARG";;
+        w) wishes="$OPTARG";;
+        h) echo "help option"; Usage;exit;;
+        \?) echo"invalid options $OPTARG"" >&2; Usage;exit;;
+        :) Usage;exit
+
+    esac    
+done
+
+if [ -z $name ] || [ -z $wishes ] 
+then
+    echo "error both -n and -w are mandatory options"
+    Usage
+    exit 1
+fi
+
+echo "hello $name $wishes. iam learning shell"
